6b39ed791d1adc9af20d8e878a97dc976232fef1
Megan/Elyott <33584689+Clepper93@users.noreply.github.com>
2018-02-08 21:03:17 -0800

    Uploaded terrible-products-sfw.py
    Uses regex functionality to filter out most blog posts that contain adult content. Ironically, the code itself is now NSFW, due to having to add swear words to the regex functions. 
    I upload this here only because the code could be useful in the future for designing a feature to limit use of banned words in other programs, such as username creation, or more advanced web scraping programs where one may wish to prevent download of explicit content.

--- terrible-products.py
+++ terrible-products.py
@@ -1,5 +1,7 @@
 import requests
 import random
+import re
+import urllib.request
 from bs4 import BeautifulSoup as bs4
 yearlist = ['2018','2017','2016','2015','2014','2013','2012']
 monthlist=['01','02','03','04','05','06','07','08','09','10','11','12']
@@ -9,18 +11,30 @@ def makeurl():
     urlmonth = random.choice(monthlist)
     urlyear = random.choice(yearlist)
     url = "http://theworstthingsforsale.com"+ '/' + urlyear + '/' + urlmonth + '/' + urlday + '/'
-    print(url)
     try:
+        urllib.request.urlopen(url).getcode()
         worstreq = requests.get(url)
         worstsoup = bs4(worstreq.text, 'html5lib')
         geth2 = worstsoup.select('h2')
         getp = worstsoup.select('p')
-        print(geth2[1].getText())
-        print(getp[2].getText())
+        ban1 = re.search(r'.*sex.* | \bbutt.* | \bass\b | .*fuck.* | \bpenis.* | .*dick.* | \bgenital.* | \bboob.* | \banus\b | .*flesh.* | \bvagina.*',getp[2].getText().lower())
+        ban2 = re.search(r'.*sex.* | \bbutt.* | \bass\b | .*fuck.* | \bpenis.* | .*dick.* | \bgenital.* | \bboob.* | \banus\b | .*flesh.* | \bvagina.*',geth2[1].getText().lower())
+        #ban2 = re.search(r'\bass\b',getp[2].getText().lower())
+        #ban3 = re.search(r'\bfuck.*',getp[2].getText().lower())
+        #ban4 = re.search(r'.*penis.*',getp[2].getText().lower())
+        #ban5 = re.search(r'.*dick.*',getp[2].getText().lower())
+        #ban6 = re.search(r'\bgenital.*',getp[2].getText().lower())
+        if ban1 or ban2:
+            print("NSFW product listing generated. Oops! Trying again...")
+            makeurl()
+        else:
+            print(url)
+            print(geth2[1].getText())
+            print(getp[2].getText())
     except:
         print("Invalid URL generated. Oops! Trying again...")
         makeurl()
-   
+    
 makeurl()
 def do_over():
     y=input("\nGenerate another terrible product? \n")
